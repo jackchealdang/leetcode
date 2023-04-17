b4421e41740b889d8fd4dc32e8c8111155f9abb0
jackchealdang <jackchealdang@gmail.com>
2023-04-16 20:10:03 -0500

    217, 242 - arrays & hashing

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for n in nums:
+            if n not in seen:
+                seen.add(n)
+            else:
+                return True
+
+        return False
+
+# @lc code=end
---
+++ 242.valid-anagram.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_letters = {}
+        t_letters = {}
+
+        for x in range(len(s)):
+            if s[x] not in s_letters:
+                s_letters.update({s[x]: 1})
+            else:
+                s_letters.update({s[x]: s_letters[s[x]]+1})
+            if t[x] not in t_letters:
+                t_letters.update({t[x]: 1})
+            else:
+                t_letters.update({t[x]: t_letters[t[x]]+1})
+
+        if s_letters == t_letters:
+            return True
+
+        return False
+
+
+# @lc code=end
